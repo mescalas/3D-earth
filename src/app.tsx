48cd0e20940db3492dae7920e6a8b6afb6a1f361
mescalas <mateo.escalas@oppus.fr>
2023-10-22 21:19:48 +0200

    try to fix build

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,8 @@
 import styles from './styles/app.module.scss'
 import {SmoothScroll} from "./components/smoothScroll"
 import {Projects} from "./components/projects";
-import dynamic from "next/dynamic";
+import {Earth} from "./components/earth";
 
-const Earth = dynamic(() => import('./components/earth').then((map) => map.Earth), {
-    ssr: false,
-    loading: () => <img src="/assets/placeholder.png"></img>
-});
 export const App = () => {
     return (
         <SmoothScroll>
